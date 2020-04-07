8342a8e32b2f36c9d8e024da9a2b3d492b4e04a0
Arnaud Gissinger <agissing@student.42.fr>
2020-04-07 17:10:44 +0200

    [AUTO-COMMIT] 2020-04-07 17:10:44.854055

--- up/install.bash
+++ up/install.bash
@@ -4,4 +4,10 @@ mkdir -p "$BASE/public" &&
 mv global up.bash now.json "$BASE/" &&
 sudo ln -s "$BASE/up.bash" "/usr/bin/up" &&
 echo "Follow now instructions" &&
-/bin/env now "$BASE"
+/bin/env now "$BASE" &&
+/usr/bin/up ls > /dev/null &&
+echo -e "\n\nInstallation done!" || (
+    echo -e "\n\nError during installation" &&
+    echo "Please consider creating an issue \
+https://github.com/mathix420/utilities/issues/new/choose"
+)
