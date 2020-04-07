1fd07fcf3931cd5d5ebbe31dfdda39c691fd279e
Arnaud Gissinger <agissing@student.42.fr>
2020-04-07 17:05:04 +0200

    [AUTO-COMMIT] 2020-04-07 17:05:04.358394

--- up/install.bash
+++ up/install.bash
@@ -2,4 +2,6 @@
 BASE="$HOME/.config/up"
 mkdir -p "$BASE/public" &&
 mv global up.bash now.json "$BASE/" &&
-sudo ln -s "$BASE/up.bash" "/usr/bin/up"
+sudo ln -s "$BASE/up.bash" "/usr/bin/up" &&
+echo "Follow now instructions" &&
+/bin/env now "$BASE"
